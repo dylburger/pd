6a67a4330031abe37c1b67da3c6a843c477ee4e0
Dylan J. Sather <dylan.sather@gmail.com>
2020-11-29 18:03:20 -0800

    Adding .gitignore, more comments

---
+++ index.js
@@ -0,0 +1,11 @@
+// Example function
+function random() {
+  return Math.random();
+}
+
+// To use any exported function, just
+// export it below and run
+// const { functionName } = require("@dylburger/pd")
+module.exports = {
+  random,
+};
